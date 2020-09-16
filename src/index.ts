f95d07e5660cbe83b69b77ebcb25612e920f6d84
Floris de Bijl <18357629+Fdebijl@users.noreply.github.com>
2020-09-16 16:12:59 +0200

    feat: add posting to channel

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,37 @@
 import dotenv from 'dotenv';
-import Discord, { TextChannel, Message, SystemChannelFlags, MessageAttachment } from 'discord.js';
-import moment from 'moment';
+import Discord, { TextChannel, MessageAttachment } from 'discord.js';
 import { Clog, LOGLEVEL} from '@fdebijl/clog';
 import Twitter, { TwitterOptions,  } from 'twitter-lite'
 
 import { CONFIG } from './config';
-import { notifyInChannel } from './notify/inChannel';
-import { notifyMods } from './notify/mods';
 
 dotenv.config();
 
 const clog = new Clog(CONFIG.MIN_LOGLEVEL);
 const client = new Discord.Client();
 client.login(process.env.BOT_TOKEN);
-
 client.on('ready', () => {
   clog.log('Bot is ready', LOGLEVEL.DEBUG);
 });
 
 const config: TwitterOptions = {
-  consumer_key: 'XYZ',
-  consumer_secret: 'XYZ',
-  access_token_key: 'XYZ',
-  access_token_secret: 'XYZ'
+  consumer_key: process.env.CONSUMER_KEY as string,
+  consumer_secret: process.env.CONSUMER_SECRET as string,
+  access_token_key: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 };
 
 const twitter = new Twitter(config);
-
 const accountsToFollow = ['957585893912731600', '33972891']
 const parameters = {
   follow: accountsToFollow.join(',')
 };
+
 const stream = twitter.stream("statuses/filter", parameters)
   .on("start", () => {
     clog.log(`Started listening for tweets from account(s): ${accountsToFollow.join(', ')}`, LOGLEVEL.DEBUG)
   })
-  .on("data", async tweet => {
+  .on("data", async (tweet: ExtendedTweet)  => {
     clog.log(`New tweet from @${tweet.user.screen_name} with ID ${tweet.id_str}`);
     const targetChannel = await client.channels.resolve(process.env.CHANNEL_ID as string);
 
@@ -44,7 +40,11 @@ const stream = twitter.stream("statuses/filter", parameters)
       return;
     }
 
-    if (tweet?.extended_entities?.media?.length === 0) {
+    if (!tweet.extended_entities) {
+      return;
+    }
+
+    if (tweet.extended_entities.media.length === 0) {
       return;
     }
 
@@ -57,3 +57,8 @@ const stream = twitter.stream("statuses/filter", parameters)
   //.on("ping", () => console.log("ping"))
   .on("error", error => console.log("error", error))
   //.on("end", response => console.log("end"));
+
+process.on('SIGTERM', () => {
+  clog.log(`Received SIGTERM, destroying stream`, LOGLEVEL.INFO);
+  process.nextTick(() => stream.destroy());
+});
--- src/notify/inChannel.ts
+++
@@ -1,9 +0,0 @@
-import { Message } from "discord.js"
-
-export const notifyInChannel = (message: Message): void => {
-  const template = process.env.IN_CHANNEL_TEMPLATE
-    ?.replace('%%TAG%%', message.author.toString())
-    ?.replace('%%MESSAGE%%', message.content)
-
-  message.channel.send(template);
-};
